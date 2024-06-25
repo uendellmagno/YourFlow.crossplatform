c85fa875b2cafc32d17720cc274f0e2b52e3cbbe
Uendell Magno <uendellmagno@gmail.com>
2024-06-25 14:07:18 -0300

    Aiming for the real app now... Added FL_Charts

--- your_flow/lib/main.dart
+++ your_flow/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:english_words/english_words.dart';
+import 'HomePage/Views/Charts/custom_line_chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,28 +12,97 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 14,
-          title: const Text(
-            "YourFlow",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            )),
-            leading: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-              color: Colors.black,
-            ),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
-          ],
-        )
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "YourFlow",
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan.shade100),
+        ),
+        home: const YFHome(),
       ),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class YFHome extends StatelessWidget {
+  const YFHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('YourFlow'),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                enableFeedback: true,
+                icon: const Icon(Icons.notifications)),
+          ],
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("I've got an awesome idea!"),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: const Text('Generate new idea'),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: const Text('Like'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const FLChart(),
+          ],
+        )));
   }
 }
